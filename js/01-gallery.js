10ce1e841f2466739522d0324fe2224a3d1fd29b
JakubJarosz <jakubtm165@gmail.com>
2022-05-24 11:41:05 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,13 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const divQuery = document.querySelector(".gallery")
+const link = galleryItems
+  .map(
+    (i) =>
+      `<div class="gallery__item"><a class ="gallery__link" href="${i.original}"><img class="gallery__image" src="${i.preview}" data-source="${i.original}" alt="${i.description}"/></a></div>`
+  )
+  .join("");
+divQuery.innerHTML = link;
+
