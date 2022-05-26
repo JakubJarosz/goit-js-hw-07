a3fb37441366e1f7e6955598f186fdd3246bee82
JakubJarosz <jakubtm165@gmail.com>
2022-05-26 15:34:20 +0200

    fix

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const divQuery = document.querySelector(".gallery");
+const link = galleryItems
+  .map(
+    (i) =>
+      `<a class ="gallery__item" href="${i.original}"><img class="gallery__image" src="${i.preview}" alt="${i.description}"/></a>`
+  )
+  .join("");
+divQuery.innerHTML = link;
+
+
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
